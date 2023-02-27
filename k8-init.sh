901c896ae0867025c9fbbb474ce8f00018a4da45
Kiran Dangol <kiran_dan2009@yahoo.com>
2023-02-26 21:21:47 -0600

    Update k8-init.sh

--- k8-init.sh
+++ k8-init.sh
@@ -86,6 +86,7 @@ sudo apt-get update
 VERSION_STRING=5:23.0.1-1~ubuntu.20.04~focal
 VERSION_STRING=5:23.0.1-1~ubuntu.18.04~bionic
 sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add your 'cloud_user' to the docker group
 
